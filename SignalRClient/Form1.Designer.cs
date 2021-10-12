15e114b609ac3d398ec10a44f92128dff06c60f8
Yancheng_Lin <Yancheng_Lin@MIS-YANCHENG_LI>
2021-10-12 16:46:32 +0800

    加入專案檔案。

---
+++ SignalRClient/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SignalRClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleButton1 = new System.Windows.Forms.Button();
+            this.simpleButton2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MessagesBlock = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(348, 228);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "全體訊息";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(429, 228);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton2.TabIndex = 0;
+            this.simpleButton2.Text = "個人訊息";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "訊息視窗";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(490, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "誰在線上";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(13, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "你想說的話";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 229);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(326, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // MessagesBlock
+            // 
+            this.MessagesBlock.Location = new System.Drawing.Point(16, 34);
+            this.MessagesBlock.Name = "MessagesBlock";
+            this.MessagesBlock.Size = new System.Drawing.Size(241, 149);
+            this.MessagesBlock.TabIndex = 10;
+            this.MessagesBlock.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(493, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(212, 148);
+            this.listBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.MessagesBlock);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox MessagesBlock;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button simpleButton1;
+         private System.Windows.Forms.Button simpleButton2;
+    }
+}
+
---
+++ SignalRClient/Form1.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignalRClient
+{
+    public partial class Form1 : Form
+    {
+
+
+        private static readonly string HubName = "BroadcastHub";
+        public HubConnection hubConnection = new HubConnection(@"http://localhost:59904/");
+
+        private IHubProxy broadcastHubProxy;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            FormClosed += Form1_FormClosed;
+            // 建立 Hub Proxy
+            this.broadcastHubProxy = this.hubConnection.CreateHubProxy(HubName);
+
+            // 註冊給伺服端呼叫的方法
+            this.broadcastHubProxy.On("ShowMessage", (string name, string message) =>
+            {
+                this.MessagesBlock.InvokeIfNecessary(() =>
+                {
+                    this.MessagesBlock.Text += ($"{name}:{message}\r\n");
+                });
+            });
+
+            this.broadcastHubProxy.On("RefreshMember", (List<string> ConnectionId) =>
+            {
+                this.listBox1.InvokeIfNecessary(() =>
+                {
+                    listBox1.Items.Clear();
+                    foreach (var Online in ConnectionId)
+                    {
+                        this.listBox1.Items.Add(Online);
+                    }
+                });
+            });
+
+            // 連線到 SignalR 伺服器
+            try
+            {
+                this.hubConnection.Start().Wait();
+                this.broadcastHubProxy.Invoke("LoginSignalR", hubConnection.ConnectionId);
+                Text = hubConnection.ConnectionId;
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                this.broadcastHubProxy.Invoke("LogOutSignalR", hubConnection.ConnectionId);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text.Replace(" ", "")))
+            {
+                this.broadcastHubProxy.Invoke("Broadcast", hubConnection.ConnectionId, "<全體>" + textBox1.Text);
+                textBox1.Text = null;
+            }
+        }
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                return;
+            }
+            if (!string.IsNullOrEmpty(textBox1.Text.Replace(" ", "")))
+            {
+                this.MessagesBlock.Text += ($"你對" + hubConnection.ConnectionId + ":<私訊>" + textBox1.Text + "\r\n");
+                this.broadcastHubProxy.Invoke("PrivateMessage", hubConnection.ConnectionId, listBox1.SelectedItem, "<私訊>" + textBox1.Text);
+                textBox1.Text = null;
+            }
+        }
+    }
+
+    static class Extension
+    {
+        public static void InvokeIfNecessary(this System.Windows.Forms.Control control, MethodInvoker action)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
